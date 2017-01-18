9f7e009a631f525a07c786de1879e61ca41a89fd
viktoreriksson <mammovick@gmail.com>
2017-01-18 12:35:08 +0100

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SpotiHelper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSong = new System.Windows.Forms.Label();
+            this.tmrSong = new System.Windows.Forms.Timer(this.components);
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.chkLog = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblSong
+            // 
+            this.lblSong.AutoSize = true;
+            this.lblSong.Location = new System.Drawing.Point(9, 76);
+            this.lblSong.Name = "lblSong";
+            this.lblSong.Size = new System.Drawing.Size(122, 13);
+            this.lblSong.TabIndex = 0;
+            this.lblSong.Text = "Song will be shown here";
+            // 
+            // tmrSong
+            // 
+            this.tmrSong.Enabled = true;
+            this.tmrSong.Interval = 1000;
+            this.tmrSong.Tick += new System.EventHandler(this.tmrSong_Tick);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(12, 12);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(224, 20);
+            this.txtLocation.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(242, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(38, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // chkLog
+            // 
+            this.chkLog.AutoSize = true;
+            this.chkLog.Location = new System.Drawing.Point(12, 38);
+            this.chkLog.Name = "chkLog";
+            this.chkLog.Size = new System.Drawing.Size(124, 17);
+            this.chkLog.TabIndex = 3;
+            this.chkLog.Text = "Enable logging to file";
+            this.chkLog.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(292, 98);
+            this.Controls.Add(this.chkLog);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.lblSong);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SpotiHelper";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSong;
+        private System.Windows.Forms.Timer tmrSong;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.CheckBox chkLog;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SpotiHelper
+{
+    public partial class frmMain : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll", SetLastError=true, CharSet=CharSet.Auto)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtLocation.Text = Properties.Settings.Default.txtLocation;
+        }
+
+        public string lastWritten = null;       //keep track of last song written to txt file, for minimal disk usage:)
+        private void tmrSong_Tick(object sender, EventArgs e)
+        {
+            IntPtr spotify = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "SpotifyMainWindow", null);
+            int spotifyTitleLength = GetWindowTextLength(spotify);
+
+            StringBuilder spotifyTitle = new StringBuilder(spotifyTitleLength);
+            GetWindowText(spotify, spotifyTitle, spotifyTitleLength + 1);                       //title of spotify, current song title
+
+            string song = null;
+            MatchCollection spotifyTitleMatch = Regex.Matches(spotifyTitle.ToString(), "^(.+) - (.+)$");
+            if (spotifyTitleMatch.Count > 0)                                                    //song playing, not an ad or is paused
+            {
+                song = lblSong.Text = spotifyTitle.ToString();
+            }
+            else
+            {
+                song = lblSong.Text = "No song currently playing";
+            }
+
+            if(song != lastWritten)     //check if current song was the last song written to txt, if not, write to disk
+            {
+                if (chkLog.Checked)     //but not before asking user for permission!
+                {
+                    File.WriteAllText(txtLocation.Text, lblSong.Text);
+                    lastWritten = song;
+                }
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.InitialDirectory = Environment.CurrentDirectory;
+                sfd.Filter = "Text Files|*.txt";
+                if (sfd.ShowDialog() == DialogResult.Cancel) return;
+                txtLocation.Text = sfd.FileName;
+                Properties.Settings.Default.txtLocation = sfd.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
